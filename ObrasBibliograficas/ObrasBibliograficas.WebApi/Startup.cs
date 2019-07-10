d773411d768c3475e02caed18fb1d201a7a86f6e
Rafael Vallin <rafa_vallin@hotmail.com>
2019-07-10 17:34:23 -0300

    Commit Alterações Angular

--- ObrasBibliograficas/ObrasBibliograficas.WebApi/Startup.cs
+++ ObrasBibliograficas/ObrasBibliograficas.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +27,10 @@ namespace ObrasBibliograficas.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSpaStaticFiles(configuration => {
+                configuration.RootPath = "ClientApp/Static";
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +46,19 @@ namespace ObrasBibliograficas.WebApi
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseMvc(mvc => {
+                mvc.MapRoute("Default", "{controller}/{action}/{id?}");
+            });
+            app.UseSpa(spa => {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer("start");
+                }
+            });
         }
     }
 }
